684a012bf92b329ff76e740b3465a4bff7372b31
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-25 00:54:49 +0100

    create Api-rest  with jsonService

--- src/app/pages/pagehome/feature-products/cart-product/cart-product.component.ts
+++ src/app/pages/pagehome/feature-products/cart-product/cart-product.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../../services/product.service';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'cart-product',
   templateUrl: './cart-product.component.html',
-  styleUrls: ['./cart-product.component.css']
+  styleUrls: ['./cart-product.component.css'],
 })
 export class CartProductComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProductNow();
   }
 
+  Detail() {
+    console.log('clicked');
+
+    console.log(this.findProductByid(1));
+  }
+  getProductNow() {
+    this.productService.getProducts().subscribe({
+      next: (products) => (this.products = products),
+      error: (err) => console.log(err),
+    });
+  }
+
+  findProductByid(id) {
+    return this.products.filter((p) => p.idProduct === id);
+  }
 }
