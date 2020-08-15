52a259d591d182f57e335895723800715133c091
DevBensmaine <bensmaine.hassan@gmail.com>
2020-08-16 00:53:29 +0100

    filter price

--- src/app/pages/page-products-filters/prix-filter/prix-filter.component.ts
+++ src/app/pages/page-products-filters/prix-filter/prix-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'prix-filter',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrixFilterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+  }
+  getPrice(){
+    let min = document.querySelector(".ac-slider__min").textContent.replace("DH", "");
+    let max = document.querySelector(".ac-slider__max").textContent.replace("DH", "");
+
+    this.productService.getProducts().subscribe((data) => {
+      this.productService.products = data;
+      this.productService.products = this.productService.products.filter((p) => p.priceAvantPromotion >= min && p.priceAvantPromotion <= max  );
+    });
   }
 
 }
